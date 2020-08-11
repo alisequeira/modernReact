7f28324ea4cf2c19ef68945221fb09ffcd048889
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-11 16:35:54 -0400

    Passing and receving props

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import CommentDetail from './CommentDetail';
 const App =  function(){
   return (
         <div className="ui container comments">
-                 <CommentDetail/>
-                 <CommentDetail/>
-                 <CommentDetail/>
-                 <CommentDetail/>
+                 <CommentDetail author="Sam"/>
+                 <CommentDetail author="Alex"/>
+                 <CommentDetail author="Jane"/>
         </div>
   );
 };
