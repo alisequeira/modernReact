63cc2de1d11965ef8642fdc7a52958a90f4e18c0
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-11 17:14:06 -0400

    Showing custom children

--- src/index.js
+++ src/index.js
@@ -3,33 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
-import AprovalCard from './AprovalCard';
 import ApprovalCard from './AprovalCard';
 //Create a react component
 const App =  function(){
   return (
         <div className="ui container comments">
-                 <ApprovalCard/>
+            <ApprovalCard>
                  <CommentDetail
                      img= {faker.image.avatar()} 
                      author="Sam" 
                      timeAgo="Today at 4:45PM" 
                      comment = "Nice blog post!"
                  />
+            </ApprovalCard>
 
+            <ApprovalCard>
                  <CommentDetail 
                      img= {faker.image.avatar()} 
                      author="Alex" 
                      timeAgo="Today at 2:00AM" 
                      comment = "HAHAHA So hilarious!"
                  />
-
+            </ApprovalCard>
+            
+            <ApprovalCard>
                  <CommentDetail 
                      img= {faker.image.avatar()}  
                      author="Jane" 
                      timeAgo="Yesterday at 5:00PM" 
                      comment = "LMAO!"
                  />
+            </ApprovalCard>
         </div>
   );
 };
